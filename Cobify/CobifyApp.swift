425165872437dbcdcae847c8c3739d99c7ba4306
Bruno Benčević <bruno.bencevic@cobeisfresh.com>
2022-11-16 23:02:29 +0100

    Initial Commit

---
+++ Cobify/CobifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobifyApp.swift
+//  Cobify
+//
+//  Created by Bruno Bencevic on 16.11.2022..
+//
+
+import SwiftUI
+
+@main
+struct CobifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
